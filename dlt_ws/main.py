73404fd628a00b6123ff895ce5e66d9602b408ce
Gamma-Pan <yiorgospanag@gmail.com>
2025-07-08 20:40:53 +0300

    dlt workshop

---
+++ dlt_ws/main.py
@@ -0,0 +1,19 @@
+import requests
+import dlt
+
+@dlt.resource()
+def zoomcamp_data():
+    docs_url = 'https://github.com/alexeygrigorev/llm-rag-workshop/raw/main/notebooks/documents.json'
+    docs_response = requests.get(docs_url)
+    documents_raw = docs_response.json()
+
+    for course in documents_raw:
+        course_name = course['course']
+
+        for doc in course['documents']:
+            doc['course'] = course_name
+            yield doc
+
+from dlt.destinations import qdrant
+
+qdrant_destination = qdrant(qd_path="db.qdrant",)
